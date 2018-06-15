b70cede03d9694a4f2941378c87b621d16260c79
Alberto Alan Zul Rabasa <alan061997@gmail.com>
2018-06-15 09:59:12 -0500

    mysql environment variables

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 
 // Connecting to the mySQL database
 const db = mysql.createConnection({
-    host : "localhost",
-    user : "alan",
+    host : process.env.MYSQL_DB_HOST,
+    user : process.env.MYSQL_DB_USER,
     password : process.env.MYSQL_DB_PASS,
-    database : "node-rest-shop"
+    database : process.env.MYSQL_DB_DATABASE
 });
 
 module.exports = db;
